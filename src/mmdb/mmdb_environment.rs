c97ba22384e10362a03a093ab3ced019ca7c5539
Josh Austin <josh.austin@gmail.com>
2017-10-30 14:52:16 -0400

    update mmdb_environment

--- src/mmdb/mmdb_environment.rs
+++ src/mmdb/mmdb_environment.rs
@@ -15,10 +15,20 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // TODO: update types of struct properties (i32 is just a placeholder)
+use std::fs::File;
+use std::path::Path;
 
 struct MmdbEnvironment {
     // data file descriptor
-    me_fd: i32,
+    data_file: File,
+    // configuration
+    config: MmdbEnvironmentConfig,
+}
+
+#[derive(Default)]
+struct MmdbEnvironmentConfig {
+    // data file path
+    data_file_path: String,
     // lock file descriptor
     me_lock_fd: i32,
     // meta page file descriptor
@@ -38,7 +48,7 @@ struct MmdbEnvironment {
     // PID of this environment
     me_pid: i32,
     // path to the DB files
-    me_path: i32,
+    me_path: String,
     // pointer to memory map of data file
     me_mmap_data: i32,
     // pointer to memory map of lock file
@@ -67,4 +77,47 @@ struct MmdbEnvironment {
     me_max_key: i32,
     // maximum value size
     me_max_val: i32,
+}
+
+impl MmdbEnvironment {
+    // construct a new MmdbEnvironment
+    pub fn open(path: &str) -> Result<MmdbEnvironment, &'static str> {
+        let db_file_path = Path::new(&path);
+        let mut file = match File::open(&db_file_path) {
+            Err(e) => return Err("error opening file"),
+            Ok(f) => f,
+        };
+        let new_env = MmdbEnvironment {
+            data_file: file,
+            config: MmdbEnvironmentConfig {
+                data_file_path: path.to_string(),
+                ..Default::default()
+            }
+        };
+        Ok(new_env)
+    }
+
+    // return data_file_path
+    pub fn get_data_file_path(&self) -> &str {
+        &self.config.data_file_path
+    }
+
+}
+
+#[cfg(test)]
+mod test {
+    extern crate rand;
+    use mmdb::rand::Rng;
+    use super::*;
+
+    #[test]
+    fn test_open() {
+        let rnd_str: String = rand::thread_rng().gen_ascii_chars().take(6).collect();
+        let path = format!("/tmp/meta_mmdb_environment_test_{}.dat", rnd_str);
+        {
+            File::create(&path);
+        }
+        let env = MmdbEnvironment::open(&path).unwrap();
+        assert_eq!(path, env.get_data_file_path());
+    }
 }
